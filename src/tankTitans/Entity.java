fbc11ba87938903f8cace4134433ee7d7e28367e
mic-hb <michael.h22@mhs.istts.ac.id>
2023-06-16 02:13:50 +0700

    Code rehaul
    Adding polymorphism

--- src/tankTitans/Entity.java
+++ src/tankTitans/Entity.java
@@ -4,6 +4,7 @@
  */
 package tankTitans;
 
+import processing.core.PApplet;
 import processing.core.PImage;
 
 /**
@@ -64,5 +65,11 @@ public abstract class Entity {
 
     public boolean is_broken() {
         return is_broken;
+    }
+
+    public void movement(boolean up, boolean down, boolean left, boolean right) {
+    }
+
+    public void drawIdle(PApplet app, int frameCtr) {
     }
 }
